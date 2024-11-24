338e87a16741568ca6d9d6d2dcca1d20097972f6
danjor667 <jordannguepi@gmail.com>
2024-11-24 13:23:11 +0100

    almost 1

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import pickle
+import numpy as np
 
 
 app = FastAPI()
@@ -9,6 +11,9 @@ app = FastAPI()
 
 with open("../ML/new_model.pkl", mode='rb') as f:
     model = pickle.load(file=f)
+
+with open("../ML/scaler.pkl", mode='rb') as f1:
+    scaler = pickle.load(file=f1)
 
 
 class InputData(BaseModel):
@@ -24,11 +29,30 @@ class InputData(BaseModel):
 
 @app.post("/predict/")
 def predict(data: InputData):
+
+    variables = ["pregnancies"
+               "glucose"
+               "Bloodpressure"
+               "SkinThickness"
+               "Insulin"
+               "BMI"
+               "DiabetesPedigreeFunction"
+               "Age"
+               ]
+
     features  = [[data.Pregnancies, data.Glucose, data.BloodPressure, data.SkinThickness, data.Insulin, data.BMI,
                  data.DiabetesPedigreeFunction, data.Age]]
+
+    ft = np.array(features)
+    ft = pd.DataFrame(ft, columns=variables)
+
+    scaled_ft = scaler.transform(ft)
+    scaled_ft = pd.DataFrame(scaled_ft, columns=variables)
+
     try:
-        prediction = model.predict(features)
-        return {"prediction": prediction}
+        prediction = model.predict(scaled_ft)
+        probability = model.predict_proba(scaled_ft)
+        return Response({"prediction": prediction, "probability": probability[0]}, status_code = 200 )
     except Exception as e:
         return {"error": str(e)}
 
@@ -36,6 +60,6 @@ def predict(data: InputData):
 
 if __name__ == "__main__":
 
-    uvicorn.run("main:app", host="0.0.0.0", port=1000, reload=true)
+    uvicorn.run("main:app", host="0.0.0.0", port=1000, reload=True)
 
 
